83aeb04cc5e249c02ce879de169972d42afea771
Maciej <maciej.tokarz1@wp.pl>
2020-11-02 19:18:33 +0100

    red test3 zadanie1

--- Lab05/src/zadanie1.py
+++ Lab05/src/zadanie1.py
@@ -2,3 +2,5 @@ class Hamming():
     def distance(self, A, B):
         if len(A) == len(B) and A == B:
             return 0
+        else:
+            return 0
--- Lab05/tests/testsZadanie1.py
+++ Lab05/tests/testsZadanie1.py
@@ -10,7 +10,7 @@ class HammingTest(unittest.TestCase):
     def test_single_letter_identical_strands(self):
         self.assertEqual(self.temp.distance("A", "A"), 0)
 
-    @unittest.skip
+
     def test_single_letter_different_strands(self):
         self.assertEqual(self.temp.distance("G", "T"), 1)
 
