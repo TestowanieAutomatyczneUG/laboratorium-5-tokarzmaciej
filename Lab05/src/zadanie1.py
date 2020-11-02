70d6b5ce453085f22837a79756b6c3c1a832268f
Maciej <maciej.tokarz1@wp.pl>
2020-11-02 19:52:40 +0100

    red test5 zadanie1

--- Lab05/src/zadanie1.py
+++ Lab05/src/zadanie1.py
@@ -1,6 +1,7 @@
 class Hamming():
     def distance(self, A, B):
-        if len(A) == len(B) and A == B:
-            return 0
-        elif A != B:
-            return 1
+        counter = 0
+        for position in range(len(A)):
+            if len(A[position]) == len(B[position]) and A[position] != B[position]:
+                counter = 1
+        return counter
