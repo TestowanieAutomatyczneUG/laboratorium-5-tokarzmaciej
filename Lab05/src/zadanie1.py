91ee921a3a38a0a053654d29aa12a29c42c4e4f3
Maciej <maciej.tokarz1@wp.pl>
2020-11-02 19:27:55 +0100

    green test3 zadanie1

--- Lab05/src/zadanie1.py
+++ Lab05/src/zadanie1.py
@@ -2,5 +2,5 @@ class Hamming():
     def distance(self, A, B):
         if len(A) == len(B) and A == B:
             return 0
-        else:
-            return 0
+        elif A != B:
+            return 1
