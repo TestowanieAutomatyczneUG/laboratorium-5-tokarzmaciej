597380da7e52f7d2342e2ffe94c49a44e8ea1353
Maciej <maciej.tokarz1@wp.pl>
2020-11-02 19:53:24 +0100

    green test5 zadanie1

--- Lab05/src/zadanie1.py
+++ Lab05/src/zadanie1.py
@@ -3,5 +3,5 @@ class Hamming():
         counter = 0
         for position in range(len(A)):
             if len(A[position]) == len(B[position]) and A[position] != B[position]:
-                counter = 1
+                counter += 1
         return counter
