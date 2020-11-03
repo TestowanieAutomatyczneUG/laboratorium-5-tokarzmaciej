85215951c7f6e274b6bd1adebbe57ab87eef1f95
Maciej <maciej.tokarz1@wp.pl>
2020-11-03 18:50:53 +0100

    red test1 zadanie3

---
+++ Lab05/tests/testsZadanie3.py
@@ -0,0 +1,27 @@
+from src.zadanie3 import ChristmasSong
+import unittest
+
+
+class SongTest(unittest.TestCase):
+
+    def test_sigle_vers_one(self):
+        self.assertEqual(self.temp.singleVers(0), 'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.')
+
+
+    # Utility functions
+    def setUp(self):
+        self.temp = ChristmasSong()
+        try:
+            self.assertRaisesRegex
+        except AttributeError:
+            self.assertRaisesRegex = self.assertRaisesRegexp
+
+    def assertRaisesWithMessage(self, exception):
+        return self.assertRaisesRegex(exception, r".+")
+
+    def tearDown(self):
+        self.temp = None
+
+
+if __name__ == '__main__':
+    unittest.main()
